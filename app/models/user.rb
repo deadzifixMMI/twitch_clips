4fe4f9e96d1c6a4bbf23189694d6feb27da39014
Deadzifix <paul.pauly@mmibordeaux.com>
2025-02-21 17:36:47 +0100

    les GOAT

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   has_many :clips, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable,
-  authentication_keys: [ :username ]
 
-  validates :username, presence: true, uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [ :username ]
+
+  validates :username, presence: true, uniqueness: { message: "est déjà pris." }
+  validates :email, presence: true, uniqueness: { message: "est déjà utilisé." }
+  # Assure-toi d'avoir une validation pour la confirmation du mot de passe si nécessaire
+  validates :password, length: { minimum: 6 }, on: :create
 end
